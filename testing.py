d522011a31ebe30f318487e4abeb17c9aa5e6ace
Arjun <144939625+Arjunmukundann@users.noreply.github.com>
2026-04-18 13:57:12 +0530

    Delete testing.py

--- testing.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = "http://localhost:5000/predict"
-files = {"file": open(r"C:\Users\Arjun\OneDrive\Desktop\ECG\ecg-app\dattasets\100.csv", "rb")}
-
-response = requests.post(url, files=files)
-
-print(response.json())
